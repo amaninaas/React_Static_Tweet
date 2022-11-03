de8580796e107e55d5f75c52ef9bbe0ae8565412
amaninaas <amaninaahmadsafri@gmail.com>
2022-11-03 13:12:58 +0800

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,70 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+function Tweet() {
+  return ( 
+    <div className="tweet">
+      <Avatar/>
+      <div className="content">
+        <NameWithHandle/>
+        <Message/>
+        <div className="buttons">
+          <ReplyButton/>
+          <RetweetButton/>
+          <LikeButton/>
+          <MoreOptionsButton/>
+        </div>
+      </div>
+    </div>
+   );
+}
+
+function Avatar() {
+  return ( 
+    <img
+    src="https://www.gravatar.com/avatar/nothing"
+    className='avatar'
+    alt='avatar'/>
+   );
+}
+
+function Message() {
+  return ( 
+    <div className="message">
+      This is less than 140 characters.
+    </div>
+   );
+}
+
+function NameWithHandle () {
+  return ( 
+    <span className="name-with-handle">
+      <span className="name">Your Name</span>
+      <span className="handle">@yourhandle</span>
+    </span>
+   );
+}
+
+const Time = ()=> (
+  <span className="time">3h ago</span>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const ReplyButton = () => (
+  <i className='fa fa-reply reply-button'/>
+);
+
+const RetweetButton = ()=> (
+  <i className='fa fa-retweet retweet-button'></i>
+);
+
+const LikeButton = ()=> (
+  <i className='fa fa-heart like-button'></i>
+);
+
+const MoreOptionsButton = ()=> (
+  <i className='fa fa-ellipsis-h more-options-button'></i>
+)
+
+
+ReactDOM.render(<Tweet/>,document.querySelector('#root'));
